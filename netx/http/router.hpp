db614a6d2dae1afbdcc998c7a999a7fa8b05e393
Your Name <you@example.com>
2026-04-02 14:01:03 +0800

    HTTP section repair completed

--- netx/http/router.hpp
+++ netx/http/router.hpp
@@ -4,11 +4,10 @@
 #include "netx/async/task.hpp"
 #include "netx/http/request.hpp"
 #include "netx/http/response.hpp"
+#include "netx/meta/radix_tree.hpp"
 #include "netx/net/stream.hpp"
 #include <cassert>
-#include <cstddef>
 #include <functional>
-#include <memory>
 #include <string>
 #include <unordered_map>
 #include <utility>
@@ -18,36 +17,12 @@ namespace http
 {
 namespace async = netx::async;
 namespace net = netx::net;
+namespace meta = netx::meta;
 using TaskType = async::Task<>;
 using HttpHandler =
 	std::function<TaskType(const HttpRequest&, HttpResponse*, net::Stream*)>;
 class HttpRouter
 {
-	struct RadixTree
-	{
-		struct RadixNode
-		{
-			HttpHandler f{nullptr};
-			std::unordered_map<std::string, std::unique_ptr<RadixNode>> next;
-		};
-
-		void insert(const std::string& path, HttpHandler handler);
-		HttpHandler search(const std::string& path);
-
-		RadixTree() : root(std::make_unique<RadixNode>())
-		{
-		}
-
-		RadixTree(RadixTree&&) = delete;
-		~RadixTree() = default;
-
-	  private:
-		std::string subPath(const std::string& path, std::size_t* start);
-
-	  private:
-		std::unique_ptr<RadixNode> root;
-	};
-
   public:
 	template <typename Handler>
 	void route(const std::string& method, const std::string& path,
@@ -64,91 +39,8 @@ class HttpRouter
 	~HttpRouter() = default;
 
   private:
-	std::unordered_map<std::string, RadixTree> trees_;
+	std::unordered_map<std::string, meta::RadixTree<HttpHandler>> trees_;
 };
-
-inline void HttpRouter::RadixTree::insert(const std::string& path,
-										  HttpHandler handler)
-{
-	if (path == "/")
-	{
-		root->f = std::move(handler);
-		return;
-	}
-
-	std::size_t start = (path[0] == '/') ? 1 : 0;
-	RadixNode* cur = root.get();
-
-	while (start != std::string::npos)
-	{
-		std::string s = subPath(path, &start);
-		if (s.empty() && start == std::string::npos)
-		{
-			break; // error
-		}
-
-		if (cur->next.find(s) == cur->next.end())
-		{
-			cur->next.try_emplace(s, std::make_unique<RadixNode>());
-		}
-
-		cur = cur->next[s].get();
-	}
-
-	cur->f = std::move(handler);
-}
-
-inline HttpHandler HttpRouter::RadixTree::search(const std::string& path)
-{
-	if (path == "/")
-	{
-		return root->f;
-	}
-
-	size_t start = (path[0] == '/') ? 1 : 0;
-	RadixNode* cur = root.get();
-
-	while (start != std::string::npos)
-	{
-		std::string s = subPath(path, &start);
-		if (s.empty() && start == std::string::npos)
-		{
-			break; // error
-		}
-
-		if (!cur->next.contains(s))
-		{
-			return nullptr; // router not exist
-		}
-
-		cur = cur->next[s].get();
-	}
-
-	return cur->f;
-}
-
-inline std::string HttpRouter::RadixTree::subPath(const std::string& path,
-												  std::size_t* start)
-{
-	// because *start maybe arive 1
-	if (*start == std::string::npos || *start >= path.size())
-	{
-		*start = std::string::npos;
-		return "";
-	}
-
-	size_t begin = *start;
-	size_t end = path.find('/', begin);
-
-	if (end != std::string::npos)
-	{
-		*start = end + 1;
-		return path.substr(begin, end - begin);
-	}
-
-	*start = std::string::npos;
-	return path.substr(begin);
-}
 
 inline TaskType HttpRouter::dispatch(const HttpRequest& req, HttpResponse* res,
 									 net::Stream* stream)
---
+++ netx/meta/radix_tree.hpp
@@ -0,0 +1,155 @@
+#ifndef NETX_META_RADIX_TREE_HPP
+#define NETX_META_RADIX_TREE_HPP
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <utility>
+namespace netx
+{
+namespace meta
+{
+template <typename T> struct RadixTree
+{
+	struct RadixNode
+	{
+		T value;
+		std::unordered_map<std::string, std::unique_ptr<RadixNode>> next;
+	};
+
+	void insert(const std::string& path, const T& val);
+	void insert(const std::string& path, T&& val);
+	T search(const std::string& path);
+
+	RadixTree() : root(std::make_unique<RadixNode>())
+	{
+	}
+
+	RadixTree(RadixTree&&) = delete;
+	~RadixTree() = default;
+
+  private:
+	std::string get_sub_path(const std::string& path, std::size_t* start);
+
+  private:
+	std::unique_ptr<RadixNode> root;
+};
+
+template <typename T>
+inline void RadixTree<T>::insert(const std::string& path, const T& val)
+{
+	if (path == "/")
+	{
+		root->value = val;
+		return;
+	}
+
+	std::size_t start = (path[0] == '/') ? 1 : 0;
+	RadixNode* cur = root.get();
+
+	while (start != std::string::npos)
+	{
+		std::string s = get_sub_path(path, &start);
+		if (s.empty() && start == std::string::npos)
+		{
+			break; // error
+		}
+
+		if (cur->next.find(s) == cur->next.end())
+		{
+			cur->next.try_emplace(s, std::make_unique<RadixNode>());
+		}
+
+		cur = cur->next[s].get();
+	}
+
+	cur->value = val;
+}
+
+template <typename T>
+inline void RadixTree<T>::insert(const std::string& path, T&& val)
+{
+	if (path == "/")
+	{
+		root->value = std::move(val);
+		return;
+	}
+
+	std::size_t start = (path[0] == '/') ? 1 : 0;
+	RadixNode* cur = root.get();
+
+	while (start != std::string::npos)
+	{
+		std::string s = get_sub_path(path, &start);
+		if (s.empty() && start == std::string::npos)
+		{
+			break; // error
+		}
+
+		if (cur->next.find(s) == cur->next.end())
+		{
+			cur->next.try_emplace(s, std::make_unique<RadixNode>());
+		}
+
+		cur = cur->next[s].get();
+	}
+
+	cur->value = std::move(val);
+}
+
+template <typename T> T RadixTree<T>::search(const std::string& path)
+{
+	if (path == "/")
+	{
+		return root->value;
+	}
+
+	size_t start = (path[0] == '/') ? 1 : 0;
+	RadixNode* cur = root.get();
+
+	while (start != std::string::npos)
+	{
+		std::string s = get_sub_path(path, &start);
+		if (s.empty() && start == std::string::npos)
+		{
+			break; // error
+		}
+
+		if (!cur->next.contains(s))
+		{
+			return nullptr; // router not exist
+		}
+
+		cur = cur->next[s].get();
+	}
+
+	return cur->value;
+}
+
+template <typename T>
+std::string RadixTree<T>::get_sub_path(const std::string& path,
+									   std::size_t* start)
+{
+	// because *start maybe arive 1
+	if (*start == std::string::npos || *start >= path.size())
+	{
+		*start = std::string::npos;
+		return "";
+	}
+
+	size_t begin = *start;
+	size_t end = path.find('/', begin);
+
+	if (end != std::string::npos)
+	{
+		*start = end + 1;
+		return path.substr(begin, end - begin);
+	}
+
+	*start = std::string::npos;
+	return path.substr(begin);
+}
+} // namespace meta
+} // namespace netx
+
+#endif
